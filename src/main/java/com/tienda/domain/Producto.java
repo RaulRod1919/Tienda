b01390d69d1c01aaa1de3d81094df0bbd36300b9
Jose Raúl Rodríguez <jrodriguez40744@ufide.ac.cr>
2025-06-16 21:35:26 -0600

    Semana #6

---
+++ src/main/java/com/tienda/domain/Producto.java
@@ -0,0 +1,42 @@
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "producto")
+public class Producto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Long idProducto;
+    @Column(name = "id_categoria")
+    private Long idCategoria;
+    private String descripcion;
+    private String detalle;
+    private double precio;
+    private int existencias;
+    @Column(name = "ruta_imagen")
+    private String rutaImagen;
+    private boolean activo;
+
+    public Producto(Long idCategoria, String descripcion, String detalle, double precio, int existencias, boolean activo) {
+        this.idCategoria = idCategoria;
+        this.descripcion = descripcion;
+        this.detalle = detalle;
+        this.precio = precio;
+        this.existencias = existencias;
+        this.activo = activo;
+    }
+
+    
+
+    public Producto() {
+    }
+
+}
