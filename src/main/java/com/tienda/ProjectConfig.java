362cd964fc94359cd0418171a1f9fe7b88ebc4b4
Jose Raúl Rodríguez <jrodriguez40744@ufide.ac.cr>
2025-08-10 15:34:18 -0600

    semana 13

--- src/main/java/com/tienda/ProjectConfig.java
+++ src/main/java/com/tienda/ProjectConfig.java
@@ -17,6 +17,8 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 /**
  *
@@ -118,4 +120,17 @@ public class ProjectConfig implements WebMvcConfigurer{
     public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception {
         build.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
+    
+    
+    @Bean
+    public SpringResourceTemplateResolver templateResolver_0() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setOrder(0);
+        resolver.setCheckExistence(true);
+        return resolver;
+    } 
+
 }
